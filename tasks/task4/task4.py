9de91af877e55c7ae36123d516334a91e675fcb8
dgraunov <dgraunov@list.ru>
2023-02-06 14:19:46 +0300

    add  sys.exit task4

--- tasks/task4/task4.py
+++ tasks/task4/task4.py
@@ -1,20 +1,28 @@
+import sys
+
+RC_OK = 0
+RC_ERROR = -1
+
 user_name, user_passw = input('login: '), input('password: ')
 if user_name == '' or user_passw == '':
     print('error')
-users_prms = {
-    'dmitry': '1234',
-    'nikita': 'qwert1',
-    'vasya': '1111'
-}
-
+    sys.exit()
 
 def _login(user_name, user_passw):
+    users_prms = {
+        'dmitry': '1234',
+        'nikita': 'qwert1',
+        'vasya': '1111'
+    }
     if user_name in users_prms and user_passw == users_prms[user_name]:
-        return f'Welcome, {user_name}'
+        return RC_OK
     else:
-        return 'Давай досвидания'
+        return RC_ERROR
 
 a = _login(user_name,user_passw)
-print(a)
+if a == 0:
+    print('Добро, пожаловать,', user_name)
+else:
+    print('Давай досвидания')
 
 

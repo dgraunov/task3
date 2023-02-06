b8c3edcd7f8e41401cfe0a9b51856aff6d7c1fd3
dgraunov <dgraunov@list.ru>
2023-02-06 15:41:56 +0300

    add except

--- tasks/task5/task5.py
+++ tasks/task5/task5.py
@@ -2,7 +2,7 @@ import sys
 
 
 conf_path = 'C:/python/tasks/task5/conf.ini'
-#Считываем файл conf.ini
+#Считываем файл conf.ini и обрабатываем исключения
 def  _read_conf():
     try:
         with open(conf_path) as f:

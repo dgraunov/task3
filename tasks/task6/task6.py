c0686d838582f8647091a3ceef6c4ed857f98677
dgraunov <dgraunov@list.ru>
2023-02-06 17:17:45 +0300

    add task6

--- tasks/task6/task6.py
+++ tasks/task6/task6.py
@@ -1,14 +1,15 @@
 conf_path = 'C:/python/tasks/task6/conf.ini'
+import re
 
 
 # Создаем функцию, которая считывает conf.ini и создает словарь
 def _read_conf():
+    data_dict = {}
     try:
         with open(conf_path) as f:
-            data_dict = {}
             for line in f:
                 k, v = line.strip().split('=')
-                if k[0] == '#':
+                if re.match(r'#', k) :
                     continue
                 elif k != '' and v != '':
                     data_dict[k.strip()] = v.strip()

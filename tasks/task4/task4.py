c7773869b05936ab378b9baaf916f16ffe648e7c
dgraunov <dgraunov@list.ru>
2023-02-06 14:36:50 +0300

    add  sys.exit task4

--- tasks/task4/task4.py
+++ tasks/task4/task4.py
@@ -1,13 +1,16 @@
 import sys
-
+#Определить константы кодов ответа
 RC_OK = 0
 RC_ERROR = -1
 
+#Запросить у пользователя ввод входных параметров
 user_name, user_passw = input('login: '), input('password: ')
+#Если на вход ничего не передали напечатать ошибку
 if user_name == '' or user_passw == '':
-    print('error')
+    print('Логин и пароль не может быть пустой строкой')
     sys.exit()
 
+#Функция проверки юзера и пароля в словаре
 def _login(user_name, user_passw):
     users_prms = {
         'dmitry': '1234',
@@ -19,8 +22,9 @@ def _login(user_name, user_passw):
     else:
         return RC_ERROR
 
-a = _login(user_name,user_passw)
-if a == 0:
+#Вызываем функцию и если код ответа 0 приветсвуем пользователя, иначе шлем его
+welcome = _login(user_name,user_passw)
+if welcome == 0:
     print('Добро, пожаловать,', user_name)
 else:
     print('Давай досвидания')

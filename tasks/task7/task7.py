44487aea3f30ab2a2a24693644c7150a85d6fb37
dgraunov <dgraunov@list.ru>
2023-02-06 15:01:11 +0300

    add task7

--- tasks/task7/task7.py
+++ tasks/task7/task7.py
@@ -1,20 +1,27 @@
+#Импортируем наш модуль с функциями
 import tools
+import sys
 
 conf_path = 'C:/python/tasks/task7/conf.ini'
 
+#Определить константы кодов ответа
+RC_OK = 0
+RC_ERROR = -1
+
+#Запросить у пользователя ввод входных параметров
 user_name, user_passw = input('login: '), input('password: ')
+
+#Если на вход ничего не передали напечатать ошибку
 if user_name == '' or user_passw == '':
-    print('error')
+    print('Логин и пароль не может быть пустой строкой')
+    sys.exit()
 
-users_prms = tools.read_conf()
+#Вызываем функцию и если код ответа 0 приветсвуем пользователя, иначе шлем его
+login_result = tools.login(user_name,user_passw)
+if login_result == RC_OK:
+    print('Добро пожаловать,', user_name)
+else:
+    print('Давай досвидания')
 
-def login(user_name, user_passw):
-    if user_name in users_prms and user_passw == users_prms[user_name]:
-        return f'Welcome, {user_name}'
-    else:
-        return 'Давай досвидания'
-
-a = login(user_name, user_passw)
-print(a)
 
 
--- tasks/task7/tools.py
+++ tasks/task7/tools.py
@@ -1,5 +1,8 @@
 conf_path = 'C:/python/tasks/task7/conf.ini'
 
+RC_OK = 0
+RC_ERROR = -1
+
 def  read_conf():
     with open(conf_path) as f:
         data_dict = {}
@@ -9,5 +12,10 @@ def  read_conf():
     return data_dict
 
 
-
+def login(user_name, user_passw):
+    users_prms = read_conf()
+    if user_name in users_prms and user_passw == users_prms[user_name]:
+        return RC_OK
+    else:
+        return RC_ERROR
 

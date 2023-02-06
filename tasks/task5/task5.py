a4f4c74c171dd5b8c1739b5815313c2fbc6ffe5a
dgraunov <dgraunov@list.ru>
2023-02-06 14:30:34 +0300

    add except

--- tasks/task5/task5.py
+++ tasks/task5/task5.py
@@ -1,7 +1,10 @@
 conf_path = 'C:/python/tasks/task5/conf.ini'
-
+#Считываем файл conf.ini
 def  _read_conf():
-    with open(conf_path) as f:
-        lines = f.read().splitlines()
+    try:
+        with open(conf_path) as f:
+            lines = f.read().splitlines()
+    except FileNotFoundError:
+        print('Файл не найден')
     return lines
 

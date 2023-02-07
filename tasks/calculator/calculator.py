bf6ad0bb09dcc26191f33ea9c002aef48181c78b
dgraunov <dgraunov@list.ru>
2023-02-07 20:53:36 +0300

    calc

--- tasks/calculator/calculator.py
+++ tasks/calculator/calculator.py
@@ -1,20 +1,26 @@
 # Запросим 2 целых числа у пользователя и действие над этими числами
 flag = True
 while flag:
-    num1 = int(input('Введите первое целое число: '))
-    num2 = int(input('Введите второе целое число: '))
+    num1 = input('Введите первое целое число: ')
+    if num1.isdigit() == False:
+        continue
+    num2 = input('Введите второе целое число: ')
+    if num2.isdigit() == False:
+        continue
     action = input('Что делаем: +, -, *, / ? ')
+    if action not in ['+', '-', '*', '/']:
+        continue
 
 # Проверяем что нужно делать и выполняем вычисление
     if action == '+':
-        print(f'{num1} + {num2} = {num1 + num2}')
+        print(f'{num1} + {num2} = {int(num1) + int(num2)}')
     elif action == '-':
-        print(f'{num1} - {num2} = {num1 - num2}')
+        print(f'{num1} - {num2} = {int(num1) - int(num2)}')
     elif action == '*':
-        print(f'{num1} * {num2} = {num1 * num2}')
+        print(f'{num1} * {num2} = {int(num1) * int(num2)}')
     elif action == '/':
         try:
-            print(f'{num1} / {num2} = {num1 / num2}')
+            print(f'{num1} / {num2} = {int(num1) / int(num2)}')
         except ZeroDivisionError:
             print('На 0 делить нельзя!!!')
     continue_calc = input('Решим еще? (y/n) ')
